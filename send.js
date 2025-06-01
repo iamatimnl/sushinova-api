4fd5fd34ec12773d00dcf2bcbc033c054b1512fb
my pj <167681477+iamatimnl@users.noreply.github.com>
2025-06-01 12:22:24 +0200

    Update send.js

--- send.js
+++ send.js
@@ -1,3 +1,4 @@
+export const config = { runtime: 'nodejs' };
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ status: 'error', error: 'Only POST requests allowed.' });
