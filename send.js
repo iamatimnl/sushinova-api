ef5a9eb8f6d9f0d01a6d9fa86cf7acb639b01773
my pj <167681477+iamatimnl@users.noreply.github.com>
2025-06-01 12:18:34 +0200

    Update send.js

--- send.js
+++ send.js
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ status: 'error', error: 'Only POST allowed' });
